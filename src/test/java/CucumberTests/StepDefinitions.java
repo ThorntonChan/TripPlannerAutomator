5f05d556e59a4aa39784ea5d4cfa9430994b3933
Thornton Chan <Thornton.Chan@transport.nsw.gov.au>
2021-10-28 21:44:28 +1100

    cucumber

--- src/test/java/CucumberTests/StepDefinitions.java
+++ src/test/java/CucumberTests/StepDefinitions.java
@@ -7,19 +7,21 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 
+import java.util.List;
+
 public class StepDefinitions {
 
-    @Given("when user is on the Australian subdirectory of the address generator")
+    @Given("user is on the Australian subdirectory of the address generator")
     public void navToAddressGenerator(){
         TransportRoute_Service.findRoutes(AddressGenerator_Service.generateAddresses("Sydney"));
     }
-    @When("the city has been selected")
-    public void selectCity(){
-        TransportRoute_Service.findRoutes(AddressGenerator_Service.generateAddresses("Sydney"));
+    @When("^the city \"(.*?)\" has been selected$")
+    public void selectCity(String cityName){
+        List<String> addresses = AddressGenerator_Service.generateAddresses(cityName);
     }
     @And("the generate button is clicked")
     public void clickGenerate(){
-        TransportRoute_Service.findRoutes(AddressGenerator_Service.generateAddresses("Sydney"));
+        TransportRoute_Service.findRoutes(addresses);
     }
     @Then("a list of addresses should be visible")
     public void retrieveAddresses(){
--- src/test/java/CucumberTests/test.feature
+++ src/test/java/CucumberTests/test.feature
@@ -1,7 +1,22 @@
 Feature: Address Generator
   Scenario: On inputting a city and clicking generate, the address generator page should return a list of addresses
 
-    Given when user is on the Australian subdirectory of the address generator
-    When the city has been selected
+    Given user is on the Australian subdirectory of the address generator
+    When the city "Sydney" has been selected
     And the generate button is clicked
     Then a list of addresses should be visible
+
+    Scenario Outline: 
+
+
+
+#    Map
+#    List
+  | city | state |
+  | Sydney | NSW |
+  | Melbourne | VIC |
+
+  | Sydney | Melbourne | Adelaide |
+
+    | city | Sydney |
+    | state | NSW |
