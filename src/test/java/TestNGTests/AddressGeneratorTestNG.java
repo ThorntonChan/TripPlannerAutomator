8d1721c758c6198cff7ad596ac1f37e7917010d4
Thornton Chan <Thornton.Chan@transport.nsw.gov.au>
2021-11-03 10:15:35 +1100

    TestNG init

---
+++ src/test/java/TestNGTests/AddressGeneratorTestNG.java
@@ -0,0 +1,22 @@
+package TestNGTests;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AddressGeneratorTestNG {
+    @BeforeClass
+    public void setup() {
+        //...
+    }
+
+    @Test
+    public void givenNumber_whenEven_thenTrue() {
+        //...
+    }
+
+    @AfterClass
+    public void tearDown() {
+        //...
+    }
+}
